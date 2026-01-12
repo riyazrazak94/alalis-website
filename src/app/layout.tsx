d4867c9981a46fa0c383b6602cb889cff09047bb
riyazrazak94 <riyazrazak94@gmail.com>
2026-01-12 14:56:48 +0300

    Update favicon to use logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/logo.png" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
